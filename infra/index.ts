4fe8bff8b65f4cb738fb77b629e91ee534b4c713
kostya9 <kostya9@gmail.com>
2020-02-11 22:29:27 +0100

    Add support of domain name to s3

--- infra/index.ts
+++ infra/index.ts
@@ -5,18 +5,76 @@ import * as fs from "fs";
 import * as mime from "mime";
 import * as path from "path";
 
-const stackConfig = new pulumi.Config("static-website");
+const stackConfig = new pulumi.Config("site");
+const domain = stackConfig.require("domain");
+const certArn = stackConfig.require("certificateArn");
 
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("wos-landing-static-server", {
+const bucket = new aws.s3.Bucket(domain, {
     acl: "public-read",
 
     // Configure S3 to serve bucket contents as a website. This way S3 will automatically convert
     // requests for "foo/" to "foo/index.html".
     website: {
         indexDocument: "index.html"
+    }
+});
+
+const cloudfrontDistribution = new aws.cloudfront.Distribution("static-woss", {
+    enabled: true,
+    aliases: [domain],
+    defaultRootObject: "index.html",
+    defaultCacheBehavior: {
+        targetOriginId: bucket.arn,
+
+        viewerProtocolPolicy: "redirect-to-https",
+        allowedMethods: ["GET", "HEAD", "OPTIONS"],
+        cachedMethods: ["GET", "HEAD", "OPTIONS"],
+
+        forwardedValues: {
+            cookies: { forward: "none" },
+            queryString: false,
+        },
+
+        minTtl: 0,
+        defaultTtl: 0,
+        maxTtl: 0,
     },
+
+    priceClass: "PriceClass_100",
+
+    // You can customize error responses. When CloudFront recieves an error from the origin (e.g. S3 or some other
+    // web service) it can return a different error code, and return the response for a different resource.
+    customErrorResponses: [
+        { errorCode: 404, responseCode: 404, responsePagePath: "/404.html" },
+    ],
+
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "none",
+        },
+    },
+
+    viewerCertificate: {
+        acmCertificateArn: certArn,  // Per AWS, ACM certificate must be in the us-east-1 region.
+        sslSupportMethod: "sni-only",
+    },
+    // We only specify one origin for this distribution, the S3 content bucket.
+    origins: [
+        {
+            originId: bucket.arn,
+            domainName: bucket.websiteEndpoint,
+            customOriginConfig: {
+                // Amazon S3 doesn't support HTTPS connections when using an S3 bucket configured as a website endpoint.
+                // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/distribution-web-values-specify.html#DownloadDistValuesOriginProtocolPolicy
+                originProtocolPolicy: "http-only",
+                httpPort: 80,
+                httpsPort: 443,
+                originSslProtocols: ["TLSv1.2"],
+            },
+        },
+    ],
 });
 
 function* recursiveFiles(dir: string): Generator<string> {
@@ -55,3 +113,4 @@ for(const relativeFilePath of recursiveFiles(webRootRelative)) {
 // Export the name of the bucket
 export const bucketName = bucket.id;
 export const bucketAddress = bucket.websiteEndpoint;
+export const domainUrl = cloudfrontDistribution.domainName;
