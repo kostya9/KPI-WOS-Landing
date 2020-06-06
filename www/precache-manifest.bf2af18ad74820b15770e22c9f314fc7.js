4ac1501dcd6e4567268e0788c85683c1fd343649
Dennis <32113099+OkhrymchukDD@users.noreply.github.com>
2020-06-06 20:44:23 +0300

    site deploy try1

---
+++ www/precache-manifest.bf2af18ad74820b15770e22c9f314fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb07f38a2b49c074ba8707c11e17324",
+    "url": "/TestGHPagesRepBooking/index.html"
+  },
+  {
+    "revision": "1871b58e790b1c09cc3b",
+    "url": "/TestGHPagesRepBooking/static/css/main.7838e538.chunk.css"
+  },
+  {
+    "revision": "8a7494d90aa34e20e2fa",
+    "url": "/TestGHPagesRepBooking/static/js/2.1d205ddd.chunk.js"
+  },
+  {
+    "revision": "dcc6d5a8d915a8b4acdc64b67831c97a",
+    "url": "/TestGHPagesRepBooking/static/js/2.1d205ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1871b58e790b1c09cc3b",
+    "url": "/TestGHPagesRepBooking/static/js/main.b510e481.chunk.js"
+  },
+  {
+    "revision": "6b46b10856b39032fd38",
+    "url": "/TestGHPagesRepBooking/static/js/runtime-main.71abf11f.js"
+  }
+]);
